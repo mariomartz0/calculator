b219ab70f8bf5e0c8554a41b5ef708aa27ffb3a0
mariomartz0 <mariomartz2018@gmail.com>
2025-06-02 17:50:02 -0400

    Create functionality to enter value into display

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
+const calculatorDisplay = document.querySelector(".calculator-display");
+calculatorDisplay.textContent = "";
+
+const calculatorBtns = document.querySelector(".calculator-btns");
+calculatorBtns.addEventListener("click", (event) => {
+  if (
+    event.target.classList.contains("operand-btn") &&
+    calculatorDisplay.textContent.length < 8
+  ) {
+    calculatorDisplay.textContent += event.target.textContent;
+  }
+  return console.log(event.target);
+});
+
+function operate(operand1, operand2, operator) {
+  let result;
+  switch (operator) {
+    case "addition":
+      result = add(+operand1, +operand2);
+      break;
+    case "subtraction":
+      result = subtract(+operand1, +operand2);
+      break;
+    case "multiplication":
+      result = multiply(+operand1, +operand2);
+      break;
+    case "division":
+      result = divide(+operand1, +operand2);
+      break;
+  }
+  console.log(result);
+}
+
 function add(operand1, operand2) {
   return operand1 + operand2;
 }
